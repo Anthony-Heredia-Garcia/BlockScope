ff51131486abc70c2c643e62245c6d9cec404af3
Anthony-Heredia-Garcia <99502244+Anthony-Heredia-Garcia@users.noreply.github.com>
2022-03-08 10:54:21 -0500

    Var vs Let vs Const

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-let name = 'Max';
+// var name = 'Max';
 
-function greet () {
-    console.log(name);
-}
-greet();
+// if (name === 'Max') {
+//     let hobbies = ['Sports', 'Cooking'];
+//     console.log(hobbies);
+// }
+
+// function greet () {
+//     var age = 30;
+//     var name = 'Manuel'
+//     console.log(name, age);
+// }
+
+// console.log(name, hobbies);
+
+// greet();
+
+console.log(name);
+
+const name = 'Max';
